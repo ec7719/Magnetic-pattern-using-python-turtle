f0293cb8ac717e26657dbb76a2d5812d423d6ee7
E.Christo Raymonde <101086026+ec7719@users.noreply.github.com>
2022-12-28 21:25:25 +0530

    Add files via upload

---
+++ Magnetic pattern using python turtle.py
@@ -0,0 +1,24 @@
+# import package
+import turtle 
+
+t1=turtle.Turtle()
+t1.pensize(3)
+t1.fillcolor("pink")
+# for default shape
+
+
+n=140
+m=200
+turtle.speed(20)
+for i in range(90):
+  t1.circle(m)
+  t1.circle(n)
+  n-=10
+  m-=20
+
+
+ 
+
+
+ 
+ 
